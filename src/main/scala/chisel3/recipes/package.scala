f66727a47b0a3a1ce790b48211c0ece0131662f5
Bryan Ngo <bryanngo@berkeley.edu>
2023-03-22 02:11:48 -0700

    make whilePrim private

--- src/main/scala/chisel3/recipes/package.scala
+++ src/main/scala/chisel3/recipes/package.scala
@@ -21,7 +21,7 @@ package object recipes {
   }
    */
 
-  def whilePrim(cond: Bool, active: Bool = Wire(Bool()))(body: Recipe*)(line: Line, fileName: FileName, enclosing: Enclosing, entity: String): Recipe = {
+  private def whilePrim(cond: Bool, active: Bool = Wire(Bool()))(body: Recipe*)(line: Line, fileName: FileName, enclosing: Enclosing, entity: String): Recipe = {
     While(cond, recipe(body:_*)(line, fileName, enclosing), active, DebugInfo(line, fileName, enclosing, entity))
   }
 
